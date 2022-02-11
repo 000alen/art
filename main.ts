9b50dfc3c906bc93ad8535b4314e0f68750cc393
Alen Rubilar <lclc.alen@gmail.com>
2022-02-11 15:15:21 -0300

    Lorem

--- main.ts
+++ main.ts
@@ -40,4 +40,9 @@ async function main() {
   // console.log(`Metadata CID: ${metadataCID}`);
 }
 
-main();
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
